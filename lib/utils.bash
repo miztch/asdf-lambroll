2a0354234ce2bad0a410d0e63531dba6eee4f7af
miztch <miztch.me@gmail.com>
2025-04-27 12:03:19 +0900

    feat: only support greater than v1.0.0

--- lib/utils.bash
+++ lib/utils.bash
@@ -27,7 +27,7 @@ sort_versions() {
 list_github_tags() {
 	git ls-remote --tags --refs "$GH_REPO" |
 		grep -o 'refs/tags/.*' | cut -d/ -f3- |
-		sed 's/^v//' # NOTE: You might want to adapt this sed to remove non-version strings from tags
+		sed 's/^v//' | grep -E '^[1-9][0-9]*\.[0-9]+\.[0-9]+(-[A-Za-z0-9.-]+)?$' # NOTE: You might want to adapt this sed to remove non-version strings from tags
 }
 
 list_all_versions() {
